ab83140a971242b60b15b8124b50b99e2abffdfc
Maksonovich <kaverron1@gmail.com>
2023-03-28 18:12:18 +0600

    prac

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.CreateRoot(document.querySelector('root'))
+
+root.render(
+    <App />
+)
